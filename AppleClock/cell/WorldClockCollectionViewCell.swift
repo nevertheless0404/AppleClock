eccbf55fe9ccdfae4bfe975253b4ad7622080013
nevertheless0404 <nevertheless.hi@gmail.com>
2024-04-14 22:16:20 +0900

    step 3

---
+++ AppleClock/cell/WorldClockCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WorldClockCollectionViewCell.swift
+//  AppleClock
+//
+//  Created by 유뇽 on 4/14/24.
+//
+
+import UIKit
+
+class WorldClockCollectionViewCell: UICollectionViewCell {
+    
+}
