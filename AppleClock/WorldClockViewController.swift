f43438774bb5fb7b3847cc56d671a7eea32134a6
nevertheless0404 <nevertheless.hi@gmail.com>
2024-05-06 14:43:26 +0900

    다시 UI 정리 해야 함

--- AppleClock/WorldClockViewController.swift
+++ AppleClock/WorldClockViewController.swift
@@ -9,25 +9,18 @@ import UIKit
 
 class WorldClockViewController: UIViewController {
 
-    
     @IBOutlet weak var worldClocktableView: UITableView!
     
     var list = [
         TimeZone(identifier: "Asia/Seoul")!,
         TimeZone(identifier: "Europe/Paris")!,
+        TimeZone(identifier: "America/New_York")!,
     ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(TimeZone.knownTimeZoneIdentifiers.count)
-        for tz in TimeZone.knownTimeZoneIdentifiers{
-            print(tz)
-        }
-
     }
-
 }
 
 extension WorldClockViewController: UITableViewDataSource {
@@ -37,6 +30,12 @@ extension WorldClockViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: WorldClockTableViewCell.self), for: indexPath) as! WorldClockTableViewCell
+        
+        let target = list[indexPath.row]
+        cell.timeLable.text = target.currentTime
+        cell.timePeriodLable.text = target.timePeriod
+        cell.timeZoneLabel.text = target.city
+        cell.timeoffsetLabel.text = target.timeOffset
         
         return cell
     }
--- Extension/TimeZone+Util.swift
+++ Extension/TimeZone+Util.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+fileprivate let formatter = DateFormatter()
+fileprivate let offsetFormatter = DateComponentsFormatter()
+
+extension TimeZone {
+    var currentTime: String? {
+        formatter.timeZone = self
+        formatter.dateFormat = "h:mm"
+        
+        return formatter.string(from: .now)
+    }
+    
+    var timePeriod: String? {
+        formatter.timeZone = self
+        formatter.dateFormat = "a"
+        
+        return formatter.string(from: .now)
+        
+    }
+    
+    var city: String? {
+        let id = identifier
+        let city = id.components(separatedBy: "/").last
+        return city 
+    }
+    
+    var timeOffset: String? {
+        //let offset = TimeZone.current.secondsFromGMT() - secondsFromGMT()
+        
+        let offset = secondsFromGMT() - TimeZone.current.secondsFromGMT()
+        let comp = DateComponents(second: offset)
+        
+        if offset.isMultiple(of: 3600){
+            offsetFormatter.allowedUnits = [.hour]
+            offsetFormatter.unitsStyle = .full
+        } else {
+            offsetFormatter.allowedUnits = [.hour, .minute]
+            offsetFormatter.unitsStyle = .positional
+        }
+        
+        let offsetStr = offsetFormatter.string(from: comp) ?? "\(offset / 3600)시간"
+        
+        let time = Date(timeIntervalSinceNow: TimeInterval(offset))
+        
+        let cal = Calendar.current
+        if cal.isDateInToday(time) {
+            return "오늘, \(offsetStr)시간"
+        } else if cal.isDateInYesterday(time) {
+            return "어제, \(offsetStr)시간"
+        } else if cal.isDateInTomorrow(time) {
+            return "내일, \(offsetStr)시간"
+        } else {
+            return nil 
+        }
+        
+    }
+}
